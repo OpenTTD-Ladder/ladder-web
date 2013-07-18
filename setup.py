b2b5bdb63dc39e7f257593b024198e978bd22fda
Xaroth Brook <devnull@xaroth.nl>
2013-07-18 11:02:42 +0200

    -Add: Prototype setup.py script for installing.
    NOTE: untested, does not work (yet)

---
+++ setup.py
@@ -0,0 +1,83 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+#
+# The package listing is kindly borrowed from the Django project:
+#   https://github.com/django/django/blob/master/setup.py
+#
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join)
+    in a platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+EXCLUDE_FROM_PACKAGES = []
+
+def is_package(package_name):
+    for pkg in EXCLUDE_FROM_PACKAGES:
+        if package_name.startswith(pkg):
+            return False
+    return True
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, package_data = [], {}
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+ladder_dir = 'ladder'
+
+for dirpath, dirnames, filenames in os.walk(ladder_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames and is_package(package_name):
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
+
+setup(
+    name='ladder-web',
+    version='0.0.1',
+    author="The OpenTTD-Ladder team",
+    author_email='not@used.now',
+    packages=packages,
+    package_data=package_data,
+    scripts=[],
+    url='https://github.com/OpenTTD-Ladder/ladder-web',
+    license='GPLv2',
+    description='TBD',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Games/Entertainment',
+        'Topic :: Games/Entertainment :: Simulation',
+    ]
+)
