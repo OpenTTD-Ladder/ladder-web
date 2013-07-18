c827d3e1a169a9cb9a614caa935dbd1ff818ec56
Xaroth Brook <devnull@xaroth.nl>
2013-07-18 23:35:12 +0200

    -Add: hook up the account urls and a mock index

--- ladder/ladder/urls.py
+++ ladder/ladder/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from django.conf import settings
 
+from util.page import TemplatePage
 
 urlpatterns = patterns('',
+    url(r'^$', TemplatePage.as_view(template_name = "base_site.html"), name = "index"),
+    url(r'^account/', include('account.urls', namespace="account")),
     # Examples:
     # url(r'^$', 'ladder.views.home', name='home'),
     # url(r'^ladder/', include('ladder.foo.urls')),
