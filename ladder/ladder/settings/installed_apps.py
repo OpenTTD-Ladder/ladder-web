f1d802a9ed1be70f08ba0be439143c66612df451
Xaroth Brook <devnull@xaroth.nl>
2013-07-18 21:31:09 +0200

    -Revert: we might actually need contrib.messages.

--- ladder/ladder/settings/installed_apps.py
+++ ladder/ladder/settings/installed_apps.py
@@ -5,6 +5,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
