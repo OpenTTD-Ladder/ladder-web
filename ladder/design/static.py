244b281b4f84a116564a9e379f6274d2f1bcdf53
Xaroth Brook <devnull@xaroth.nl>
2013-07-18 16:10:02 +0200

    -Add: aforementioned modules to global js/css

--- ladder/design/static.py
+++ ladder/design/static.py
@@ -1,9 +1,15 @@
 global_js = [
-
+    (-100,  'jquery/jquery-1.9.1.js'),
+    (-99,   'underscore/underscore.js'),
+    (-98,   'jquery.lazyload/jquery.lazyload.js'),
+    (-95,   'bootstrap/bootstrap.js'),
+    (-90,   'mustache/mustache.js'),
+    (-1,    'holder/holder.js'),
 ]
 
 global_css = [
-
+    (-99,   'bootstrap/bootstrap.css'),
+    (-98,   'fontawesome/font-awesome.css'),
 ]
 
 compile_js = [
@@ -11,5 +17,5 @@ compile_js = [
 ]
 
 compile_css = [
-
+    'fontawesome/font-awesome.less',
 ]
