eed277f2a02471c4b97cb1cc898b23ab62425bf7
Xaroth Brook <devnull@xaroth.nl>
2013-07-19 11:28:23 +0200

    -Cleanup: urlconf had some crap in it.

--- ladder/ladder/urls.py
+++ ladder/ladder/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.conf import settings
 
 from util.page import TemplatePage
@@ -7,15 +6,7 @@ from util.page import TemplatePage
 urlpatterns = patterns('',
     url(r'^$', TemplatePage.as_view(template_name = "base_site.html"), name = "index"),
     url(r'^account/', include('account.urls', namespace="account")),
-    # Examples:
-    # url(r'^$', 'ladder.views.home', name='home'),
-    # url(r'^ladder/', include('ladder.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
 
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
