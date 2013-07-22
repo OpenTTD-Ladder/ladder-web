f157ec223cffe265300a864d990d4cd7103456f5
Xaroth Brook <devnull@xaroth.nl>
2013-07-22 09:54:33 +0200

    -Change: add some display logic.

--- ladder/frontpage/models.py
+++ ladder/frontpage/models.py
@@ -12,7 +12,18 @@ class News(Translatable):
     author          = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = "news_authored")
     authored        = models.DateTimeField(default = timezone.now)
 
+    class Meta:
+        verbose_name = "News Item"
+        verbose_name_plural = "News Items"
+
 class NewsTranslation(Translation):
     news            = models.ForeignKey(News, related_name='translations')
     title           = models.CharField(max_length = 64)
     long_desc       = RichTextField()
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Translation"
+        verbose_name_plural = "Translations"
