f54de073a0bf4cb456d22563db5d1ab4311594e0
Xaroth Brook <devnull@xaroth.nl>
2013-07-24 15:27:18 +0200

    -Change: enable translation middleware.

--- ladder/ladder/settings/middlewares.py
+++ ladder/ladder/settings/middlewares.py
@@ -1,6 +1,7 @@
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'translations.middleware.TranslationMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
