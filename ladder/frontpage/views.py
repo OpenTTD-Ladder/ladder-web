a1bb703347ed89693487921640b0572a3557020f
Xaroth Brook <devnull@xaroth.nl>
2013-07-24 18:31:05 +0200

    -Add: Ladders on the front page.

--- ladder/frontpage/views.py
+++ ladder/frontpage/views.py
@@ -1,14 +1,19 @@
+from django.utils import timezone
+
 from util.page import Page
 
 from .models import News
+
+from matchmaking.models import Ladder
 
 class Frontpage(Page):
     template_name = "frontpage/frontpage.html"
 
     def get(self, request):
-        news = list(News.objects.all().order_by('-authored')[:5])
+        news = list(News.objects.filter(authored__lte = timezone.now()).prefetch_related('author').order_by('-authored')[:5])
         return self.render_to_response({
             'news': news,
+            'ladders': Ladder.objects.get_ladders_list(20),
             })
 
 class NewsItem(Page):
